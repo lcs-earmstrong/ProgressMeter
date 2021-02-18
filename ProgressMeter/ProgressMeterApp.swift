1c2a5313cb55e2291e72c0debf807b48a8cd487a
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-18 09:12:39 -0500

    Initial Commit

---
+++ ProgressMeter/ProgressMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressMeterApp.swift
+//  ProgressMeter
+//
+//  Created by Evan Armstrong on 2021-02-18.
+//
+
+import SwiftUI
+
+@main
+struct ProgressMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

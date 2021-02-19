368b808109cc90596bf4552b7aa58b5fbd32c716
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-19 07:41:44 -0500

    Almost done just need to add spacer views

--- ProgressMeter/ContentView.swift
+++ ProgressMeter/ContentView.swift
@@ -8,42 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     // For driving animation to reveal rectangle with progress meter fill
-        @State private var progressMeterOffset = CGSize.zero
+    @State private var progressMeterOffset = CGSize.zero
+    
+    // Width of the meter
+       let meterWidth: CGFloat = 100
+
+       // Thickness of meter's border
+       let borderWidth: CGFloat = 2
+       
+       // Padding at top of progress meter
+       let verticalPadding: CGFloat = 44
+    
     var body: some View {
-        VStack {
-            ZStack{
+        GeometryReader { geometry in
             
-            Rectangle()
-                .frame(width: 100, height: 548 - 44, alignment: .center)
-                .onAppear(perform: {
-                    // Offset is moves the opaque rectangle up
-                    progressMeterOffset = CGSize(width: 0, height: -1 * (548 - 44))
-                    
-                })
-            // Will slide up
-            Rectangle()
-                    .fill(Color.primary)
-                    .colorInvert()
-                    .frame(width: 100, height: 548 - 44, alignment: .center)
-                .offset(progressMeterOffset)
-                .onAppear(perform: {
-                    // Offset is moves the opaque rectangle up
-                    progressMeterOffset = CGSize(width: 0, height: -1 * (548 - 44))
-                })
-                // Sits above the rectangle that slides up (in the z-axis)
-                // This means the rectangle sliding up will pass beneath this view
-                Rectangle()
-                    .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
-                    .frame(width: 100 + 2, height: 548 - 44 + 2, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(Color.primary, lineWidth: 2)
-                    )
+            Text("Height is \(geometry.size.height)")
+            
+           // VStack {
+              //  ZStack {
+                    // "Fill" for progress meter; stationary
+                //    Rectangle()
+                  //      .frame(width: meterWidth, height: 548 - verticalPadding, alignment: .center)
+
+                    // Will slide up
+                    //Rectangle()
+//                        .fill(Color.primary)
+//                        .colorInvert()
+//                        .frame(width: meterWidth, height: 548 - verticalPadding, alignment: .center)
+//                        .offset(progressMeterOffset)
+//                        .onAppear(perform: {
+//                            withAnimation(Animation.easeIn(duration: 4.0)) {
+//                                // Offset is moves the opaque rectangle up
+//                                progressMeterOffset = CGSize(width: 0, height: -1 * (548 - verticalPadding))
+//                            }
+//                        })
+//
+//                    // Sits above the rectangle that slides up (in the z-axis)
+//                    // This means the rectangle sliding up will pass beneath this view
+//                    Rectangle()
+//                        .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
+//                        .frame(width: meterWidth + borderWidth, height: 548 - verticalPadding + borderWidth, alignment: .center)
+//                        .overlay(
+//                            Rectangle()
+//                                .stroke(Color.primary, lineWidth: borderWidth)
+//                        )
+//
+//                }
+//
+//            }
+//
         }
+        
     }
-}
-}
+   }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

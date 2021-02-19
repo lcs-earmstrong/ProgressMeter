a5502304b6a64d3481945d968dcd7d38b22ab2c7
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-19 09:03:57 -0500

    Finished black and white progress meter

--- ProgressMeter/ContentView.swift
+++ ProgressMeter/ContentView.swift
@@ -24,45 +24,50 @@ struct ContentView: View {
     var body: some View {
         GeometryReader { geometry in
             
-            Text("Height is \(geometry.size.height)")
-            
-           // VStack {
-              //  ZStack {
-                    // "Fill" for progress meter; stationary
-                //    Rectangle()
-                  //      .frame(width: meterWidth, height: 548 - verticalPadding, alignment: .center)
+            VStack {
+                
+                Spacer()
+                
+                HStack{
+                
+                Spacer()
+                    
+                ZStack {
+                   //  "Fill" for progress meter; stationary
+                    Rectangle()
+                        .frame(width: meterWidth, height: geometry.size.height - verticalPadding, alignment: .center)
 
-                    // Will slide up
-                    //Rectangle()
-//                        .fill(Color.primary)
-//                        .colorInvert()
-//                        .frame(width: meterWidth, height: 548 - verticalPadding, alignment: .center)
-//                        .offset(progressMeterOffset)
-//                        .onAppear(perform: {
-//                            withAnimation(Animation.easeIn(duration: 4.0)) {
-//                                // Offset is moves the opaque rectangle up
-//                                progressMeterOffset = CGSize(width: 0, height: -1 * (548 - verticalPadding))
-//                            }
-//                        })
-//
-//                    // Sits above the rectangle that slides up (in the z-axis)
-//                    // This means the rectangle sliding up will pass beneath this view
-//                    Rectangle()
-//                        .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
-//                        .frame(width: meterWidth + borderWidth, height: 548 - verticalPadding + borderWidth, alignment: .center)
-//                        .overlay(
-//                            Rectangle()
-//                                .stroke(Color.primary, lineWidth: borderWidth)
-//                        )
-//
-//                }
-//
-//            }
-//
+                   //  Will slide up
+                    Rectangle()
+                        .fill(Color.primary)
+                        .colorInvert()
+                        .frame(width: meterWidth, height: geometry.size.height - verticalPadding, alignment: .center)
+                        .offset(progressMeterOffset)
+                        .onAppear(perform: {
+                            withAnimation(Animation.easeIn(duration: 4.0)) {
+                                // Offset is moves the opaque rectangle up
+                                progressMeterOffset = CGSize(width: 0, height: -1 * (geometry.size.height - verticalPadding))
+                            }
+                        })
+
+                    // Sits above the rectangle that slides up (in the z-axis)
+                    // This means the rectangle sliding up will pass beneath this view
+                    Rectangle()
+                        .fill(Color(hue: 0, saturation: 0, brightness: 0, opacity: 0))
+                        .frame(width: meterWidth + borderWidth, height: geometry.size.height - verticalPadding + borderWidth, alignment: .center)
+                        .overlay(
+                            Rectangle()
+                                .stroke(Color.primary, lineWidth: borderWidth)
+                        )
+Spacer()
+                }
+Spacer()
+            }
+
         }
-        
     }
    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
